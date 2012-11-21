58b487ba49294d25f3649b39eecbd2096519beb2
Peter Pilgerstorfer <peter@pilgerstorfer.at>
2012-11-22 00:57:21 +0100

    sdf

--- src/Assistant.java
+++ src/Assistant.java
@@ -8,43 +8,4 @@ public class Assistant extends Servant{
 	public Assistant(int serialNum) {
 		super(serialNum);
 	}
-	
-	@Override
-	public ValidationCode validSkin(TouchSensitiveSkin skin) {
-		// TODO Auto-generated method stub
-		return super.validSkin(skin);
-	}
-	
-	@Override
-	public ValidationCode validSkin(ArmoredSkin skin) {
-		// TODO Auto-generated method stub
-		return super.validSkin(skin);
-	}
-	
-	@Override
-	public ValidationCode validSkin(SolidSkin skin) {
-		// TODO Auto-generated method stub
-		return super.validSkin(skin);
-	}
-	
-	@Override
-	public ValidationCode validMainType(Servant other) {
-		// TODO Auto-generated method stub
-		return super.validMainType(other);
-	}
-	
-	@Override
-	public ValidationCode validMainType(Guardian other) {
-		// TODO Auto-generated method stub
-		return super.validMainType(other);
-	}
-	
-	@Override
-	public ValidationCode validMainType(HeavyWorker other) {
-		// TODO Auto-generated method stub
-		return super.validMainType(other);
-	}
-	
-	
-
 }
