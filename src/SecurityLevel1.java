6b053f3a9ac4c16f02ac5b8bc9f3de6e168ed064
koegleal-tu <koegleal_foren@hotmail.com>
2012-11-22 01:33:35 +0100

    sile1-5

---
+++ src/SecurityLevel1.java
@@ -0,0 +1,10 @@
+
+public class SecurityLevel1 implements SecurityLevel{
+
+	@Override
+	public ValidationCode validateAndroid(Android android) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
