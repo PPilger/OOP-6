d40f1f4e15e7cd7d89cca19ad55e39f660f88735
koegleal-tu <koegleal_foren@hotmail.com>
2012-11-22 01:12:24 +0100

    jkl

--- src/Assistant.java
+++ src/Assistant.java
@@ -8,4 +8,14 @@ public class Assistant extends Servant{
 	public Assistant(int serialNum) {
 		super(serialNum);
 	}
+	
+	public class BaseSoftware implements Software
+	{
+
+		@Override
+		public ValidationCode validateAndroid(Android android) {
+			return null;
+		}
+		
+	}
 }
--- src/Partner.java
+++ src/Partner.java
@@ -3,9 +3,19 @@
  * @author Koegler Alexander
  *
  */
-public class Partner extends Servant{
-
+public class Partner extends Servant
+{
+	public class BaseSoftware implements Software
+	{
+		@Override
+		public ValidationCode validateAndroid(Android android) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
+	
 	public Partner(int serialNum) {
 		super(serialNum);
+		// TODO Auto-generated constructor stub
 	}
 }
