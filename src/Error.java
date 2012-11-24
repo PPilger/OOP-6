8b7958de5ba8b76ea1d0ee29ef5c9ddf6f5fb584
PPilger <peter@pilgerstorfer.at>
2012-11-24 16:58:05 +0100

    alle fehlermeldungen ausgebbar

--- src/Error.java
+++ src/Error.java
@@ -1,24 +1,49 @@
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class Error implements ValidationCode {
-	private String message;
+	private Set<String> messages = new HashSet<String>();
 	
 	public Error(String message) {
-		this.message = message;
+		messages.add(message);
 	}
 	
 	@Override
 	public ValidationCode mergeOr(ValidationCode other) {
+		return other.mergeOr(this); // visit mergeOr(Error) of the other validationCode
+	}
+	
+	@Override
+	public ValidationCode mergeOr(Error other) {
+		messages.addAll(other.messages);
+		return this;
+	}
+	
+	@Override
+	public ValidationCode mergeOr(Valid other) {
 		return other;
 	}
 	
 	@Override
 	public ValidationCode mergeAnd(ValidationCode other) {
+		return other.mergeOr(this); // visit mergeAnd(Error) of the other validationCode
+	}
+	
+	@Override
+	public ValidationCode mergeAnd(Error other) {
+		messages.addAll(other.messages);
+		return this;
+	}
+	
+	@Override
+	public ValidationCode mergeAnd(Valid other) {
 		return this;
 	}
 	
 	@Override
 	public String toString() {
-		return message;
+		return messages.toString();
 	}
 
 	@Override
--- src/ValidationCode.java
+++ src/ValidationCode.java
@@ -1,10 +1,20 @@
-
 public interface ValidationCode {
 	public ValidationCode mergeOr(ValidationCode other);
+
+	public ValidationCode mergeOr(Error other);
+
+	public ValidationCode mergeOr(Valid other);
+
 	public ValidationCode mergeAnd(ValidationCode other);
+
+	public ValidationCode mergeAnd(Error other);
+
+	public ValidationCode mergeAnd(Valid other);
+
 	public ValidationCode executeIfValid(Operation op);
+
 	public String toString();
-	
+
 	public static interface Operation {
 		public void execute();
 	}
